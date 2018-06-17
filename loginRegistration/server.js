867395dc9519b1ed9b71b2d0a7a72854d1a9ead9
isidagreat <isidagreat@gmail.com>
2018-06-16 17:51:04 -0700

    Added Validations

--- loginRegistration/server.js
+++ loginRegistration/server.js
@@ -4,7 +4,7 @@ var express = require('express');
 var app = express();
 // Require the use of mongoose
 var mongoose = require('mongoose');
-mongoose.connect('mongodb://localhost/quotes');
+mongoose.connect('mongodb://localhost/users');
 // Require body-parser(to receive post data from clients)
 var bodyParser = require('body-parser');
 // Integrate body-parser with our app
@@ -26,14 +26,16 @@ app.use(session({
     saveUninitialized:true,
     cookie:{maxAge:60000}
 }));
+const flash = require('express-flash');
+app.use(flash());
 // setup bcrypt
 const bcrypt = require('bcrypt-as-promised');
 // Create MOngoose schemas
 var UserSchema = new mongoose.Schema({
-    email: String,
-    fname: String,
-    lname: String,
-    hash: String,
+    email: {type:String, min:7, required:true},
+    fname: {type: String,min:3, required: true},
+    lname: {type: String,min:3, required: true},
+    hash: {type: String,min:8, required: true},
     bday: Date
 });
 mongoose.model('User', UserSchema); // We are setting this Shema in our models as USER
@@ -49,45 +51,87 @@ app.get('/', function(req, res){
 });
 // Add Quote Request
 app.post('/register', function(req,res){
-    console.log("POST DATA", req.body);
-    // first hash password
-    bcrypt.hash(req.body.password, 10)
-    .then(hashed_password => {
-         
-    })
-    .catch(error => {
-         
-    });
-    // Create a a new Quote with the name and age
-    var user = new User({fname: req.body.fname,lname: req.body.lname, email: req.body.email, hash:req.body.password, bday: req.body.bday});
-    // Try and save that user to the database(this method that actually insets intot the DB)
-    user.save(function(err){
-        if(err){
-            console.log("Something went Wrong");
-        } else{
-            console.log("successfully Created User");
-            res.redirect('/success');
+    var count = 0;
+    User.count({email:req.body.email}, (err, user) => {
+        if (err || user === 0) {
+           
+            bcrypt.hash(req.body.password, 10)
+            .then(hashed_password => {
+            // Create a a new Quote with the name and age
+            var user = new User({fname: req.body.fname,lname: req.body.lname, email: req.body.email, hash:hashed_password, bday: req.body.bday});
+            // Try and save that user to the database(this method that actually insets intot the DB)
+            user.save(function(err){
+                if(err){
+                    console.log("Something went Wrong",err);
+                    for(var key in err.errors){
+                        req.flash('registration', err.errors[key].message);
+                    }
+                    // redirect the user to an appropriate route
+                    res.redirect('/');
+                } else{
+                    console.log("successfully Created User");
+                    res.redirect('/success');
+                }
+            }); 
+            })
+            .catch(error => {
+                pass(error)
+        
+            });
+
+
+
+        }else{
+            console.log("TOOO MANY")
+            res.redirect('/')
         }
-    });
+        
+
+}) 
+
+    
+
+
+
 });
 app.post('/login', (req, res) => {
-    console.log(" req.body: ", req.body);
-    User.findOne({email:req.body.email, password: req.body.password}, (err, user) => {
+    User.findOne({email:req.body.email}, (err, user) => {
         if (err) {
             // Code...
-        }
-        else {
-            // Code...
-    	req.session.id = User._id;
-        req.session.email = User.email;
-        res.redirect('/success');
-        }
+            console.log("Something Went Wrong locattin user")
+            res.redirect('/')
+        }else{
+        bcrypt.compare(req.body.password, user.hash)
+        .then( result => {
+            console.log(result)
+            req.session.id = user._id;
+            req.session.email = user.email;
+            res.redirect('/success');
+        })
+        .catch( error => {
+            console.log("OHHHHHH OHHHH BAD STUFF HAPPEND"+error);
+            res.redirect('/')
+
+})
+}
+        // else {
+        //     // Code...
+    	// req.session.id = user._id;
+        // req.session.email = user.email;
+        // res.redirect('/success');
+        // }
     })
 })
 app.get('/success',function(req,res){
+    if (!req.session.id){
+        res.redirect('/');
+    }
+    else{
+        console.log(req.session.id)
     res.render('success');
+    }
 } );
 // Set server to listen on Port 8000
-app.listen(8000, function(){
+app.listen(8001, function(){
     console.log("listening on port 8000")
 });
